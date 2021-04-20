0008dddcef6b2b1f401cc10fd92a66ec84479b5f
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-04-20 09:40:57 +0300

    good work styled also

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ function loadImg() {
     $.each(data, function(i,f) {
       $('#select').append('<option value="'+f.keyword+'">'+f.keyword+'</option>');
       var value = $('#select').val();
-      if(value == '0'){
+      if(value === '0'){
         $('#content').append('<div><img height="80" width="100" src="' + f.image_url + '">  </div>');
       }
-      if(value == f.keyword){
+      if(value === f.keyword){
         $('#content').append('<div><img height="80" width="100" src="' + f.image_url + '">  </div>');
       }
     });
