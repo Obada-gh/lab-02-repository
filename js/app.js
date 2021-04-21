e12e144d996dde3cba983be20dcbe13dc932b864
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-04-21 03:11:48 +0300

    lab03 nice work

--- js/app.js
+++ js/app.js
@@ -1,45 +1,27 @@
 'use strict';
 
-$('#select').change(function(){
-  $( '#content' ).empty();
+$('#select').change(function () {
+  $('#content').empty();
   loadImg();
 });
 
 function loadImg() {
   $.getJSON('data/page-1.json', function (data) {
-    $.each(data,function(index,item){
-      $('#select').append('<option value="'+item.keyword+'">'+item.keyword+'</option>');
+    $.each(data, function (index, item) {
+      $('#select').append('<option  value="' + item.keyword + '">' + item.keyword + '</option>');
       var html = ''
-          + '<div class="dataItem">'
-              + '<p>{{title}} <br> <img src={{image_url}} alt="Girl in a jacket" width="100" height="100"> <br>{{keyword}} <br> {{horns}} </p>'
-          + '</div>';
-      $('#name').click(function(){
-        item.sort(function(a, b) {
-          var nameA = a.title.toUpperCase(); // ignore upper and lowercase
-          var nameB = b.title.toUpperCase(); // ignore upper and lowercase
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
+        + '<div class="dataItem" sortbytitle="{{title}}" sortbyid="{{horns}}">'
+        + '<p>{{title}} <br> <img src={{image_url}} alt="Girl in a jacket" width="50" height="50"> <br>{{keyword}}  <br> {{horns}} </p>'
+        + '</div>';
 
-          // names must be equal
-          return 0;
-        });
-      });
-      $('#num').click(function(){
-        item.sort(function (a, b) {
-          return a.horns - b.horns;
-        });
-      });
+
       var value = $('#select').val();
-      if(value === '0'){
-        $('#content').append(Mustache.render(html,item));
+      if (value === '0') {
+        $('#content').append(Mustache.render(html, item));
 
       }
-      if(value === item.keyword){
-        $('#content').append(Mustache.render(html,item));
+      if (value === item.keyword) {
+        $('#content').append(Mustache.render(html, item));
       }
 
 
@@ -47,3 +29,24 @@ function loadImg() {
   });
 }
 
+$('#sort').change(function () {
+
+  var value = $('#sort').val();
+  var items = $('.dataItem');
+  if(value==='name'){
+    items.sort(function(a, b){
+      var aText= a.attributes.sortbytitle.value.toUpperCase();
+      var bText =b.attributes.sortbytitle.value.toUpperCase();
+      return (aText < bText) ? -1 : (aText > bText) ? 1 : 0;
+    });
+  }else{
+    items.sort(function(a, b){
+      var aText= parseInt(a.attributes.sortbyid.value);
+      var bText =parseInt(b.attributes.sortbyid.value);
+      return (aText < bText) ? -1 : (aText > bText) ? 1 : 0;
+    });
+  }
+  $('#content').html('');
+  $('#content').html(items);
+
+});
--- js/app2.js
+++ js/app2.js
@@ -1,48 +1,52 @@
 'use strict';
 
-$('#select').change(function(){
-  $( '#content' ).empty();
+$('#select').change(function () {
+  $('#content').empty();
   loadImg();
 });
 
 function loadImg() {
   $.getJSON('data/page-2.json', function (data) {
-    $.each(data,function(index,item){
-      $('#select').append('<option value="'+item.keyword+'">'+item.keyword+'</option>');
-      $('#name').click(function(){
-        item.sort(function(a, b) {
-          var nameA = a.title.toUpperCase(); // ignore upper and lowercase
-          var nameB = b.title.toUpperCase(); // ignore upper and lowercase
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-
-          // names must be equal
-          return 0;
-        });
-      });
-      $('#num').click(function(){
-        item.sort(function (a, b) {
-          return a.horns - b.horns;
-        });
-      });
+    $.each(data, function (index, item) {
+      $('#select').append('<option  value="' + item.keyword + '">' + item.keyword + '</option>');
       var html = ''
-          + '<div class="dataItem">'
-              + '<p>{{title}} <br> <img src={{image_url}} alt="Girl in a jacket" width="100" height="100"> <br>{{keyword}} <br> {{horns}} </p>'
-          + '</div>';
+        + '<div class="dataItem" sortbytitle="{{title}}" sortbyid="{{horns}}">'
+        + '<p>{{title}} <br> <img src={{image_url}} alt="Girl in a jacket" width="50" height="50"> <br>{{keyword}}  <br> {{horns}} </p>'
+        + '</div>';
+
+
       var value = $('#select').val();
-      if(value === '0'){
-        $('#content').append(Mustache.render(html,item));
+      if (value === '0') {
+        $('#content').append(Mustache.render(html, item));
 
       }
-      if(value === item.keyword){
-        $('#content').append(Mustache.render(html,item));
+      if (value === item.keyword) {
+        $('#content').append(Mustache.render(html, item));
       }
 
 
     });
   });
 }
+
+$('#sort').change(function () {
+
+  var value = $('#sort').val();
+  var items = $('.dataItem');
+  if(value==='name'){
+    items.sort(function(a, b){
+      var aText= a.attributes.sortbytitle.value.toUpperCase();
+      var bText =b.attributes.sortbytitle.value.toUpperCase();
+      return (aText < bText) ? -1 : (aText > bText) ? 1 : 0;
+    });
+  }else{
+    items.sort(function(a, b){
+      var aText= parseInt(a.attributes.sortbyid.value);
+      var bText =parseInt(b.attributes.sortbyid.value);
+      return (aText < bText) ? -1 : (aText > bText) ? 1 : 0;
+    });
+  }
+  $('#content').html('');
+  $('#content').html(items);
+
+});
